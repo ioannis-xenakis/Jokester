7b8e1b8084d0d850fba22242f7c05292f85997c9
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2022-07-31 14:46:58 +0300

    Remove unused imported libraries from SetupNavGraph.kt

--- app/src/main/java/com/john_xenakis/jokester/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/john_xenakis/jokester/navigation/SetupNavGraph.kt
@@ -1,10 +1,7 @@
 package com.john_xenakis.jokester.navigation
 
 import android.os.Build
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
