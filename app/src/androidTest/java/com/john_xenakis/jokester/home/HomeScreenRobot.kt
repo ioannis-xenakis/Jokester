ce37fc7b80c58a79ddff111c63951c58262d44e7
ioannis xenakis <Xenakis.i.contact@gmail.com>
2022-12-17 20:13:24 +0200

    Add license section to source code files

--- app/src/androidTest/java/com/john_xenakis/jokester/home/HomeScreenRobot.kt
+++ app/src/androidTest/java/com/john_xenakis/jokester/home/HomeScreenRobot.kt
@@ -5,6 +5,27 @@ import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.john_xenakis.jokester.MainActivity
 
+/*
+    Jokester is the app for reading jokes and make people laugh.
+    Copyright (C) 2022  Ioannis Xenakis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Anything you want to contact me for, contact me with an e-mail, at: xenakis.i.contact@gmail.com
+    I'll be happy to help you, or discuss anything with you!
+ */
+
 /**
  * The connection link that is used for reaching and using HomeScreenRobot class
  * that in turn homeScreenRobot function is used in Home Screen tests.
--- app/src/androidTest/java/com/john_xenakis/jokester/webmock/ErrorDispatcher.kt
+++ app/src/androidTest/java/com/john_xenakis/jokester/webmock/ErrorDispatcher.kt
@@ -7,6 +7,27 @@ import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
 
+/*
+    Jokester is the app for reading jokes and make people laugh.
+    Copyright (C) 2022  Ioannis Xenakis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Anything you want to contact me for, contact me with an e-mail, at: xenakis.i.contact@gmail.com
+    I'll be happy to help you, or discuss anything with you!
+ */
+
 /**
  * The JSON response file name,
  * that is responsible,
